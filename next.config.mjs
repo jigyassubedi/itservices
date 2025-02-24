92ff5e75142512601bbf7a086aba846570e4683a
jigyassubedi <103407134+jigyassubedi@users.noreply.github.com>
2025-02-24 23:37:45 +0530

    deupdate

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'out',
+  output: "export",
   images: {
-    unoptimized: true,
+    unoptimized: true, // Required for GitHub Pages
   },
+  basePath: "/itservices", // Important for GitHub Pages
 };
-
 module.exports = nextConfig;
